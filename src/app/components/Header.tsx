e2db1b787f2ed0d099bb5522082ba19bb9fdbf65
TakayukiHirnao117 <ikusa.hirano@icloud.com>
2024-12-03 17:55:18 +0900

    ✨ shadcnの導入、heroコンポーネントの作成

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 const Header = () => {
     return (
-        <header className="flex justify-between h-16 bg-[#1d4ed8] items-center px-4 text-slate-50 shadow-lg">
-            <h1 className="font-black text-lg">BlueZz&apos;s portfolio</h1>
+        <header className="flex justify-between h-16 bg-[#334155] items-center px-4 text-slate-50">
+            <h1 className="font-black text-lg text-white opacity-100">BlueZz&apos;s portfolio</h1>
             <nav>
                 <ul className="flex gap-4 ">
                     <li>
